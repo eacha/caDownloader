de53cb617ac8294a6c34fac7dfb38e303dee4a5b
Eduardo Acha <eduardoacha@gmail.com>
2016-04-11 11:44:25 -0400

    Use the correct import

--- main.go
+++ main.go
@@ -1,11 +1,12 @@
 package main
 
-import "certificate-transparency/transparency"
+import "github.com/eacha/ct-downloader/transparency"
 
 func main() {
 	for i:= range transparency.LogServers {
 		url := transparency.LogServers[i]
 		logServerConnection := transparency.New(url, 1000)
+
 		if logServerConnection == nil{
 			continue
 		}
